c36dbc5a0f6b59fadd3b8127f253cb6c590ee131
Anirudh Pabbarju <anirudhpabbaraju1103@gmail.com>
2024-08-26 12:01:17 +0530

    back

--- backend/application/config.py
+++ backend/application/config.py
@@ -6,6 +6,10 @@ class Config():
     SQLITE_DB_DIR=None
     SQLALCHEMY_DATABASE_URI=None
     SQLALCHEMY_TRACK_MODIFIATIONS=False
+    CELERY_BROKER_URL="redis://localhost:6379/0"
+    CELERY_RESULT_BACKEND="redis://localhost:6379/0"
+    broker_connection_retry_on_startup=True 
+    
     
 class LocalDevelopmentConfig(Config):
     SQLITE_DB_DIR=os.path.join(cur_directory,"../db_directory")
